182393b094f93ccc3ee055d3a41f0bf480592d50
royderks <hello@hackteam.io>
2018-06-28 20:45:05 +0200

    Inladen van data vanuit Firebase

--- Screens/Home.js
+++ Screens/Home.js
@@ -2,29 +2,24 @@ import React from 'react';
 import { FlatList, Image, ScrollView, Text, View } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import { createStackNavigator } from 'react-navigation';
+import * as firebase from 'firebase';
 
 import { Categories } from '../Components/Categories';
 import { Listings } from '../Components/Listings';
 import { Listing } from './Listing';
 
 class HomeScreen extends React.Component {
-  render() {
-    const categories = [
-      { "key": "1", "title": "Woningen", "image": "https://a0.muscache.com/im/pictures/8b7519ec-2c82-4c09-8233-fd4d2715bbf9.jpg?aki_policy=small" },
-      { "key": "2", "title": "Ervaringen", "image": "https://a0.muscache.com/im/pictures/cb8b3101-d419-4c17-8e2f-4989b39b98c3.jpg?aki_policy=small" },
-      { "key": "3", "title": "Restaurants", "image": "https://a0.muscache.com/im/pictures/da2d8e97-90b7-409f-94ac-5ab0327c289b.jpg?aki_policy=small" }
-    ];
+  componentDidMount() {
+    firebase.database().ref('categories').once('value', (snapshot) => {
+      this.setState({ categories: snapshot.val() })
+    });
+    firebase.database().ref('listings').once('value', (snapshot) => {
+      this.setState({ listings: snapshot.val() })
+    });
+  }
 
-    const listings = [
-      { "key": "1", "title": "I Sette Coni - Trullo Edera", "image": "https://a0.muscache.com/im/pictures/15273358/d7329e9a_original.jpg?aki_policy=small" },
-      { "key": "2", "title": "Luxury Gold Coast Hinterland Villa", "image": "https://a0.muscache.com/im/pictures/7787384/03380852_original.jpg?aki_policy=small" },
-      { "key": "3", "title": "Luxurious stone villa in Crete", "image": "https://a0.muscache.com/im/pictures/17588525/94250ea3_original.jpg?aki_policy=small" },
-      { "key": "4", "title": "Best location design quiet AC free wifi citycenter", "image": "https://a0.muscache.com/im/pictures/6061582/a643208f_original.jpg?aki_policy=small" },
-      { "key": "5", "title": "I Sette Coni - Trullo Edera", "image": "https://a0.muscache.com/im/pictures/15273358/d7329e9a_original.jpg?aki_policy=small" },
-      { "key": "6", "title": "Luxury Gold Coast Hinterland Villa", "image": "https://a0.muscache.com/im/pictures/7787384/03380852_original.jpg?aki_policy=small" },
-      { "key": "7", "title": "Luxurious stone villa in Crete", "image": "https://a0.muscache.com/im/pictures/17588525/94250ea3_original.jpg?aki_policy=small" },
-      { "key": "8", "title": "Best location design quiet AC free wifi citycenter", "image": "https://a0.muscache.com/im/pictures/6061582/a643208f_original.jpg?aki_policy=small" }
-    ];
+  render() {
+    const { categories, listings } = (this.state) ? this.state : {};
 
     return (
       <View style={styles.container}>
