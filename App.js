c38df3a6f272df2f1d42dd9afc04f204b9c7ca00
royderks <hello@hackteam.io>
2018-06-28 20:34:23 +0200

    Firebase installeren en initialiseren

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import { createBottomTabNavigator } from 'react-navigation';
+import * as firebase from 'firebase';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import { FontAwesome } from '@expo/vector-icons';
 
 import Home from './Screens/Home';
 import Profile from './Screens/Profile';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: 'API_KEY>',
+  authDomain: '<AUTH_DOMAIN',
+  databaseURL: '<DATBASE_URL>',
+  storageBucket: '<STORAGE_BUCKET>'
+};
+
+firebase.initializeApp(firebaseConfig);
 
 EStyleSheet.build();
 
