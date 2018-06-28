886ea9625b59914822572d12c249b8606fcdb305
royderks <hello@hackteam.io>
2018-06-28 19:31:19 +0200

    Splits de twee nieuwe Components in nieuwe bestanden, importeer deze in App.js

--- Screens/Home.js
+++ Screens/Home.js
@@ -2,47 +2,8 @@ import React from 'react';
 import { FlatList, Image, ScrollView, Text, View } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
-const Categories = ({ data }) => {
-  return (<View>
-    <Text style={ styles.subHeading }>Waar ben je naar op zoek?</Text>
-    <ScrollView horizontal showsHorizontalScrollIndicator={ false }>
-      <FlatList
-          style={ styles.categoryList }
-          horizontal
-          data={ data }
-          renderItem={ ({ item, index }) => <View style={ EStyleSheet.child(styles, 'category', index, 3) }>
-            <Image
-              style={ styles.categoryImage }
-              source={{ uri: item.image }}
-            />
-            <Text style={ styles.categoryText }>{ item.title }</Text>
-          </View> }
-        />
-      </ScrollView>
-  </View>)
-}
-
-const Listings = ({ data }) => {
-  return (
-    <View>
-      <Text style={ styles.subHeading }>Woningen over de hele wereld</Text>
-      <ScrollView>
-        <FlatList
-          numColumns={ 2 }
-          style={ styles.listingList }
-          data={ data }
-          renderItem={ ({ item, index }) => <View style={ EStyleSheet.child(styles, 'listing', index, 8) }>
-            <Image
-              style={ styles.listingImage }
-              source={{ uri: item.image }}
-            />
-            <Text style={ styles.listingText }>{ item.title }</Text>
-          </View> }
-        />
-      </ScrollView>
-    </View>
-  )
-}
+import { Categories } from '../Components/Categories';
+import { Listings } from '../Components/Listings';
 
 export default class Home extends React.Component {
   render() {
@@ -80,59 +41,5 @@ const styles = EStyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  subHeading: {
-    fontSize: '1.2rem',
-    fontWeight: 'bold',
-    margin: 10,
-  },
-  categoryList: {
-    flex: 1,
-    width: '100%'
-  },
-  category: {
-    width: 150,
-    borderWidth: 1,
-    borderColor: '#ddd',
-    borderRadius: 5,
-    marginLeft: 10,
-    marginVertical: 10,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 0 },
-    shadowOpacity: 0.2,
-    shadowRadius: 2,
-    elevation: 2
-  },
-  'category:last-child': {
-    marginRight: 10
-  },
-  categoryImage: {
-    borderRadius: 5,
-    width: 148,
-    height: 98
-  },
-  categoryText: {
-    padding: 5,
-    shadowOpacity: 0
-  },
-  listingList: {
-    flex: 1,
-    width: '100%'
-  },
-  listing: {
-    flex: 1,
-    marginLeft: 10,
-    marginVertical: 10,
-  },
-  'listing:nth-child-odd': {
-    marginRight: 10
-  },
-  listingImage: {
-    borderRadius: 5,
-    width: '100%',
-    height: 120
-  },
-  listingText: {
-    paddingVertical: 5
   }
 });
