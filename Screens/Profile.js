20b7f09ae351a3d129645e0808aecadfacee9682
royderks <hello@hackteam.io>
2018-06-28 20:12:50 +0200

    Vraag om toestemming voor het gebruik van de CameraRoll

--- Screens/Profile.js
+++ Screens/Profile.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
+import { Permissions } from 'expo';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
 export default class Profile extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      hasCameraRollPermission: null,
+    };
+  }
+
+  pickImage = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    this.setState({ hasCameraRollPermission: status === 'granted' });
+
+    if (status === 'granted') {
+      console.log('We kunnen!')
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Profiel</Text>
+        <Button title='Vraag om toestemming' onPress={ this.pickImage } />
       </View>
     );
   }
