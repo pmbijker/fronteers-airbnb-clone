303741582a0adbef8c06a554b6a31af81a5727aa
royderks <hello@hackteam.io>
2018-06-28 18:44:27 +0200

    Toevoegen van meer opties aan de BottomNavigator

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createBottomTabNavigator } from 'react-navigation';
+import { FontAwesome } from '@expo/vector-icons';
 
 class App extends React.Component {
   render() {
@@ -17,6 +18,23 @@ class App extends React.Component {
 export default createBottomTabNavigator(
   {
     Home: App // Koppel het component App aan de route Home
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        switch (routeName) {
+          case 'Home':
+            iconName = 'search';
+        }
+        return <FontAwesome name={ iconName } size={ 25 } color={ tintColor } />;
+      }
+    }),
+    tabBarOptions: {
+      activeTintColor: '#fd5c63',
+      inactiveTintColor: 'gray',
+    },
   }
 );
 
